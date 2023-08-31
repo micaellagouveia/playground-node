3ccb619131191d12e55bfe1476eaba6ff3c2204d
micaellagouveia <micaella2212@gmail.com>
2023-08-31 19:11:04 -0300

    feat: config db and sequelize

---
+++ src/db/migrations/20230831220634-create_table_user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    return await queryInterface.createTable('user', { id: Sequelize.INTEGER });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return await queryInterface.dropTable('user');
+  }
+};
