204caab5acee249a5efe49a2612c8c43f4c63daa
micaellagouveia <micaella2212@gmail.com>
2023-09-22 15:28:15 -0300

    refactor fetch data 7.8

--- models/product.js
+++ models/product.js
@@ -1,42 +1,32 @@
 const path = require('path');
 const fs = require('fs');
-const products = [];
+const p = path.join(path.join(__dirname, '..', 'data', 'products.json'));
 
-module.exports = class Product {
-  constructor(
-    title
-    // id, name, price, description, image
-  ) {
-    this.title = title;
-    // this.id = id;
-    // this.name = name;
-    // this.price = price;
-    // this.description = description;
-    // this.image = image;
-  }
-  save() {
-    const p = path.join(path.join(__dirname, '..', 'data', 'products.json'));
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => console.log(err));
-    }
-    );
-  }
-
-  // o static serve para que não seja necessário instanciar a classe para chamar o método
-  static fetchAll(cb) {
-    const p = path.join(path.join(__dirname, '..', 'data', 'products.json'));
-
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
       cb(JSON.parse(fileContent));
     }
-    );
+  });
+};
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
   }
-}
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
